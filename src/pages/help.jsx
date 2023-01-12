c73695dc0dde005f0ed828ccfc128d298de8e79f
Joel Dario <joeldario9912@gmail.com>
2023-01-12 16:05:05 -0300

    modifique titulo de index.html, agregue iconos y modifique estilo a botonoes de pagina help

--- src/pages/help.jsx
+++ src/pages/help.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../styles/help.css';
-import { BsInfoCircleFill } from "react-icons/bs";
+import { BsInfoCircleFill, BsArrowLeftSquareFill } from "react-icons/bs";
 
 const Help = () => {
   return (
     <div className='help-container'>
+      <a href="/"><BsArrowLeftSquareFill /></a>
       <BsInfoCircleFill className='help-icon' />
       <h2>Ayuda</h2>
       <p>Luego de escribir el nombre, pulsa Enter o el boton <i>Agregar</i>.</p>
       <p>Pulsa sobre cualquier nombre de la lista para eliminarlo.</p>
       <p>Pulsa <i>Resetear</i> para borrar todos los nombres de la lista.</p>
       <p>Una vez hecho el sorteo, puedes recargar la página para obtener un sorteo diferente.</p>
-      <a href="/">hecho</a>
     </div>
   );
 }
